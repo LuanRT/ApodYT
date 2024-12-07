56c854f5d11431d05132f8d9c2fb43940fa8abe1
Luan <luan.lrt4@gmail.com>
2024-12-07 06:29:21 -0300

    chore: update deps

--- src/main.ts
+++ src/main.ts
@@ -10,77 +10,76 @@ import VideoFactory from './utils/VideoFactory';
 // @ts-ignore
 import getDuration from 'mp3-duration';
 
-(async () => {
-  const innertube = await Innertube.create({
-    cache: new UniversalCache(false),
-    retrieve_player: false
-  });
+const innertube = await Innertube.create({
+  cache: new UniversalCache(false),
+  retrieve_player: false
+});
 
-  innertube.session.on('auth', () => console.info('Successfully logged in.'));
-  innertube.session.on('update-credentials', () => console.info('Credentials updated.'));
-  innertube.session.on('auth-pending', (data) => console.info(data));
+innertube.session.on('auth', () => console.info('Successfully logged in.'));
+innertube.session.on('update-credentials', () => console.info('Credentials updated.'));
+innertube.session.on('auth-pending', (data) => console.info(data));
 
-  const { ACCESS_TOKEN, REFRESH_TOKEN, EXPIRES } = process.env;
+const { ACCESS_TOKEN, REFRESH_TOKEN, EXPIRES } = process.env;
 
-  const oauthTokens = Reflect.has(process.env, 'ACCESS_TOKEN') ? {
-    access_token: ACCESS_TOKEN,
-    refresh_token: REFRESH_TOKEN,
-    expiry_date: EXPIRES
-  } as OAuth2Tokens : undefined;
+const oauthTokens = Reflect.has(process.env, 'ACCESS_TOKEN') ? {
+  access_token: ACCESS_TOKEN,
+  refresh_token: REFRESH_TOKEN,
+  expiry_date: EXPIRES
+} as OAuth2Tokens : undefined;
 
-  await innertube.session.signIn(oauthTokens);
+await innertube.session.signIn(oauthTokens);
 
-  const info = await innertube.account.getInfo();
+const info = await innertube.account.getInfo();
 
-  console.info('Account info:', info);
+console.info('Account info:', info);
 
-  const apod = await NasaApi.getAPOD();
+const apod = await NasaApi.getAPOD();
 
-  console.info('APOD:', apod);
+console.info('APOD:', apod);
 
-  const image = await downloadImage(apod.media_type === 'image' ? apod.url : apod.thumbnail_url);
-  const audio = await TTSApi.speak(apod.explanation, `${tmpdir()}/${Date.now()}.mp3`);
-  const duration = await getDuration(audio);
+const image = await downloadImage(apod.media_type === 'image' ? apod.url : apod.thumbnail_url);
+const audio = await TTSApi.speak(apod.explanation, `${tmpdir()}/${Date.now()}.mp3`);
+const duration = await getDuration(audio);
 
-  const gen_video = await VideoFactory.render([ {
-    path: image,
-    caption: apod.title,
-    captionStart: 4000,
-    captionEnd: 11000
-  } ], {
-    path: audio, duration
-  });
+const gen_video = await VideoFactory.render([ {
+  path: image,
+  caption: apod.title,
+  captionStart: 4000,
+  captionEnd: 11000
+} ], {
+  path: audio, duration
+});
 
-  const file = fs.readFileSync(gen_video);
+const file = fs.readFileSync(gen_video);
 
-  const nasa_credit =
-    `Image URL: ${apod.url || 'N/A'}\n\n` +
-    'This can also be found at: https://apod.nasa.gov/apod/astropix.html\n' +
-    `Credit & Copyright: ${apod?.copyright || 'N/A'}`;
+const nasa_credit =
+  `Image URL: ${apod.url || 'N/A'}\n\n` +
+  'This can also be found at: https://apod.nasa.gov/apod/astropix.html\n' +
+  `Credit & Copyright: ${apod?.copyright || 'N/A'}`;
 
-  const description = `${apod.explanation}\n\n${nasa_credit}`;
+const description = `${apod.explanation}\n\n${nasa_credit}`;
 
-  const upload = await innertube.studio.upload(file.buffer, {
-    title: apod.title,
-    description: description,
-    privacy: 'PUBLIC'
-  });
+const upload = await innertube.studio.upload(file.buffer, {
+  title: apod.title,
+  description: description,
+  privacy: 'PUBLIC'
+});
 
-  await innertube.studio.updateVideoMetadata(upload.data.videoId, {
-    tags: [
-      'Astronomy',
-      'Science',
-      'NASA',
-      'APOD',
-      'Space',
-      'Automated'
-    ],
-    category: 27, // Education
-    license: 'creative_commons'
-  });
+console.info(`Successfully uploaded "${apod.title}"!`);
 
-  console.info(`Successfully uploaded "${apod.title}"!`);
-})();
+await innertube.studio.updateVideoMetadata(upload.data.videoId, {
+  tags: [
+    'Astronomy',
+    'Science',
+    'NASA',
+    'APOD',
+    'Space',
+    'Automated'
+  ],
+  category: 27, // Education
+  license: 'creative_commons'
+});
+
 
 async function downloadImage(url: string): Promise<string> {
   const response = await fetch(url);
